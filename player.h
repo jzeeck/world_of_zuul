532f1f0182e08081032e974400c9ad3fa54a3c89
Jonas af Sandeberg <jonas.kth@gmail.com>
2012-12-11 13:32:37 +0100

    Player can equip armor and weapon. He can also have a vector with consumables.

--- player.h
+++ player.h
@@ -2,32 +2,30 @@
 #define PLAYER_H_
 
 #include "Items/item.h"
+#include "Items/armor.h"
+#include "Items/weapon.h"
+#include "Items/consumable.h"
 #include "Map/Tiles/tile.h"
 #include <string>
 
 class Player {
 private:
 	std::string l_name;
-	//const static unsigned int max_number_of_items = 8;
-
 	Tile* current_tile;
 
-	/*int l_strength;
-	int l_dexterity;
-	int l_vitality;
-
-	int l_experience;*/
 	int l_hp;
 	bool has_talked_to_paladin;
 
-	/*Item* l_head_gear;
-	Item* l_wepond;
-	Item* l_shield;
-	Item* l_chest;
-	Item* l_gloves;
-	Item* l_boots;
+	Armor* l_head_gear;
+	Weapon* l_weapon;
+	Armor* l_shield;
+	Armor* l_chest;
+	Armor* l_gloves;
+	Armor* l_boots;
 
-	Item* l_inventory[8];*/
+	std::vector <Consumable *> l_consumables;
+	void init_start_items(void);
+
 public:
 	void set_name(std::string var);
 	Player();
@@ -40,6 +38,18 @@ public:
 	int get_hp(void);
 	unsigned int do_damage(void);
 	void talked_to_paladin(void);
+
+	// functions for items
+	void equip_weapon(Weapon * weapon);
+	void equip_armor(Armor * armor);
+	void use_consumable(unsigned int);
+
+	void pickup_consumable(Consumable * consumable);
+	void print_consumables();
+	void print_equipped();
+
+	int get_protection();
+
 };
 
 #endif /* PLAYER_H_ */
